a151f223a3edcd6acf4b25373ecc96b5201a2250
麻荣雨 <@192.168.110.215>
2019-08-27 22:46:36 +0800

    v1.0

---
+++ fileupload/src/main/java/controller/testController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.File;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+@Controller
+public class testController {
+
+	@RequestMapping("sss")
+	public  @ResponseBody String aaa(@RequestParam("file") CommonsMultipartFile file,HttpServletRequest req )throws Exception {
+		String oname=file.getOriginalFilename();
+		String ex=oname.substring(oname.lastIndexOf("."),oname.length());
+		String nname = UUID.randomUUID()+ex;
+  
+		String path=req.getSession().getServletContext().getRealPath("");
+		path=new File(path).getParentFile().getPath()+"\\upload";
+		System.out.println(path);
+		file.transferTo(new File(path,nname));
+		return "/upload/"+nname;
+	}
+	
+//	@RequestMapping("login1")
+//	@RequiresPermissions("user")
+//	public void login(String name,String pass) {
+//		Subject sub = SecurityUtils.getSubject();
+//		UsernamePasswordToken token = new UsernamePasswordToken(name,pass);
+//		System.out.println("¿ªÊ¼µÇÂ½");
+//		 sub.login(token);    
+//	}
+	
+//	@RequestMapping("index")
+//	public void index() {
+//		System.out.println("index");
+//	}
+}
