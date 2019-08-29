559ee558f5ad6b59bd4f6bf00c05b134f0e9e1d0
麻荣雨 <@192.168.110.215>
2019-08-29 20:44:30 +0800

    v1.1

--- project/src/main/java/controller/TestController.java
+++ project/src/main/java/controller/TestController.java
@@ -1,15 +1,19 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
 public class TestController {
 	
 	@RequestMapping("index")
-	public String test() {
-		return "index";
+	public void test(@RequestParam(defaultValue="1")int txt,ModelMap m) {
+		//return "index";
+		m.put("info", txt);
+		
 	}
 	
 }
